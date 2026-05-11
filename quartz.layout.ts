a65f9bca27a55773f97c214aa5a5157983e1ef6d
heleme69 <lehuyme9@gmail.com>
2026-05-11 17:44:00 +0700

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Download PDFs (GitHub)": "https://github.com/heleme69/MyObsidian/tree/main/Content",
+      "My Main Blog": "https://heleme69.github.io/MyBlog/", 
     },
   }),
 }
